c929c3a4c962c7507b4d8152b7bd07fde53852ab
SergeyL <mkru@mail.ru>
2022-08-27 13:51:46 +0300

    Add Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Значения кубов чисел от 1 до {n} равны:");
+if(n >= 0){
+    for(int i = 1; i <= n; i++){
+    Console.Write($"{Math.Pow(i, 3)}, ");
+    }
+}
+else{   // Для отрицательного числа n 
+    for(int i = 1; i >= n; i--){
+    Console.Write($"{Math.Pow(i, 3)}, ");
+    }
+}
+Console.WriteLine("");
